f80a11f997896054c5e6e6d58c7ebde62cab48c9
David O'Dey <david.odey@pearson.com>
2017-03-26 12:58:10 -0700

    saving

--- pattern-lab/source/js/main.js
+++ pattern-lab/source/js/main.js
@@ -1,32 +1,30 @@
 const openBtn = document.getElementById('openMainMenu'),
-      barsIcon = document.getElementById('bars'),
-      barsIcon2 = document.getElementById('bars2'),
-      sidebar = document.getElementById('sidebar'),
-      content = document.getElementById('content'),
-      overlay = document.getElementById('overlay'),
-      firstNavItem = document.getElementById('openMainMenu2'),
-      lastNavButton = document.querySelector('#dropdownNav li:last-child button'),
-      lastListItem = document.querySelector('#dropdownNav li:last-child button + div ul li:last-child a'),
-      list = document.querySelector('#dropdownNav li:last-child button + div ul li:last-child'),
-      anchor = document.querySelectorAll('#sidebar a'),
-      buttons = document.querySelectorAll('#sidebar button');
+    barsIcon = document.getElementById('bars'),
+    barsIconInMenu = document.getElementById('barsInMenu'),
+    sidebar = document.getElementById('sidebar'),
+    content = document.getElementById('content'),
+    overlay = document.getElementById('overlay'),
+    firstNavItem = document.getElementById('openMainMenu2'),
+    lastNavButton = document.querySelector('#dropdownNav li:last-child button'),
+    lastListItem = document.querySelector('#dropdownNav li:last-child button + div ul li:last-child a'),
+    secondLastListItem = document.getElementById("three");
 
-let   timer = 0,
-      counter = 0;
+let timer = 0,
+    counter = 0;
 
 
-// closes the menu
+// helper that closes the menu
 function closeMenu() {
     sidebar.classList.remove('open');
     overlay.classList.remove('overlay');
-    setTimeout(function(){
+    setTimeout(() => {
         sidebar.classList.add('js-hide');
         openBtn.focus();
     }, 500)
 }
 
 
-// toggles the data-focus attribute
+// helper that toggles the data-focus attribute
 function toggleDataFocus(event) {
     const target = event.target;
     if (target.getAttribute('data-focused') === 'false') {
@@ -38,8 +36,8 @@ function toggleDataFocus(event) {
 
 
 // close the menu when escape is pressed
-window.onkeydown = function(event) {
-    if (event.key === "Escape" || event.key === 'Esc'){
+window.onkeydown = (event) => {
+    if (event.key === "Escape" || event.key === 'Esc') {
         if (sidebar.classList.contains('open')) {
             closeMenu();
         }
@@ -47,46 +45,60 @@ window.onkeydown = function(event) {
 };
 
 
-// captures focus and toggles focus
-sidebar.addEventListener('focus', function(event) {
+// keyboard focus and blur
+// cycle through menu
+sidebar.addEventListener('focus', (event) => {
     toggleDataFocus(event);
-        firstNavItem.onkeydown = function(event) {
-            if (lastNavButton.getAttribute('aria-expanded') === 'false' && event.shiftKey === true && event.key === "Tab") {
-                setTimeout(function(){
-                    lastNavButton.focus();
-                }, 10)
-            } else if (lastNavButton.getAttribute('aria-expanded') === 'true' && event.shiftKey === true && event.key === "Tab") {
-                setTimeout(function(){
-                    lastListItem.focus();
-                }, 10)
-            }
-        };
+    firstNavItem.onkeydown = (event) => {
+        const ariaExpanded = lastNavButton.getAttribute('aria-expanded'),
+            shiftKey = event.shiftKey,
+            keyPress = event.key;
 
-        lastNavButton.onkeydown = function(event) {
-            if (event.target.getAttribute('aria-expanded') === 'false' && event.shiftKey === true && event.key === "Tab") {
-                setTimeout(function(){
-                    document.getElementById("Compounds").focus();
-                }, 10)
-            }
-        };
-
-        lastListItem.onkeydown = function(event) {
-            if (event.shiftKey === true && event.key === "Tab") {
-                setTimeout(function(){
-                    document.getElementById("three").focus();
-                }, 10)
-            }
-
+        if (ariaExpanded === 'false' && shiftKey === true && keyPress === "Tab") {
+            setTimeout(() => {
+                lastNavButton.focus();
+            }, 10)
+        } else if (ariaExpanded === 'true' && shiftKey === true && keyPress === "Tab") {
+            setTimeout(() => {
+                lastListItem.focus();
+            }, 10)
         }
+    };
+
+    lastNavButton.onkeydown = (event) => {
+        const ariaExpanded = event.target.getAttribute('aria-expanded'),
+            shiftKey = event.shiftKey,
+            keyPress = event.key;
+
+        if (ariaExpanded === 'false' && shiftKey === true && keyPress === "Tab") {
+            setTimeout(() => {
+                document.getElementById("Compounds").focus();
+            }, 10)
+        }
+    };
+
+    lastListItem.onkeydown = (event) => {
+        const shiftKey = event.shiftKey,
+            keyPress = event.key;
+
+        if (shiftKey === true && keyPress === "Tab") {
+            setTimeout(() => {
+                document.getElementById("three").focus();
+            }, 10)
+        }
+
+    }
 
 }, true);
 
 
-// captures blur and toggles blur
-sidebar.addEventListener('blur', function(event) {
-    const target = event.target;
+// keyboard toggle aria expanded attributes
+sidebar.addEventListener('blur', (event) => {
+    const target = event.target,
+        sidebarOpen = sidebar.classList.contains('open');
+
     toggleDataFocus(event);
-    if (sidebar.classList.contains('open') === true) {
+    if (sidebarOpen === true) {
         if (target === lastNavButton && target.getAttribute('aria-expanded') === 'false') {
             firstNavItem.focus();
         } else if (lastNavButton.getAttribute('aria-expanded') === 'true' && target === lastListItem) {
@@ -96,11 +108,12 @@ sidebar.addEventListener('blur', function(event) {
 }, true);
 
 
-// toggles the menu
+// opens the menu and sets focus to first item.
+// when the overlay is clicked, close the menu and set focus to button.
 content.addEventListener('click', event => {
     if (event.target === barsIcon || event.target === openBtn) {
         sidebar.classList.remove('js-hide');
-        setTimeout(function(){
+        setTimeout(() => {
             sidebar.classList.add('open');
             overlay.classList.add('overlay');
             firstNavItem.focus();
@@ -113,27 +126,27 @@ content.addEventListener('click', event => {
 });
 
 
-// toggles submenu items
+// toggles aria expanded attributes when item is clicked.
+// closes the menu when bars are clicked.
 sidebar.addEventListener('click', event => {
     const ariaExpanded = event.target.getAttribute('aria-expanded');
     if (ariaExpanded === 'false') {
         event.target.setAttribute('aria-expanded', 'true');
     } else if (ariaExpanded === 'true') {
         event.target.setAttribute('aria-expanded', 'false');
-    } else if (event.target === firstNavItem || event.target === barsIcon2) {
+    } else if (event.target === firstNavItem || event.target === barsIconInMenu) {
         closeMenu();
     }
 
 });
 
 
-
 // changes the menu icon color at a certain scroll position
 content.addEventListener('scroll', event => {
-    if(timer) {
+    if (timer) {
         window.clearTimeout(timer);
     }
-    timer = window.setTimeout( ()=> {
+    timer = window.setTimeout(() => {
         const position = event.target.scrollTop;
         if (position >= 331) {
             barsIcon.classList.add('primary');
