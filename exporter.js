abe37a74382ef77857334dcc902d3d3ed8bef538
Parker Malenke <parker.malenke@pearson.com>
2018-03-12 17:06:46 -0600

    Add export scripts

---
+++ exporter.js
@@ -0,0 +1,124 @@
+// 1. Create components file
+// 2. Read component IDs
+// 3. Insert assets
+// 4. Create versions file
+
+var fs = require('fs')
+var yaml = require('js-yaml')
+var http = require('http')
+
+function slugify(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
+
+// Create core data structure
+var data = {}
+
+var folders = fs.readdirSync('./c')
+folders.forEach(folder => {
+  if (folder == '.DS_Store') return
+
+  var versions = fs.readdirSync('./c/' + folder)
+
+  versions.forEach(version => {
+    if (version.indexOf('v') !== 0) return
+
+    try {
+      var file = './c/' + folder + '/' + version + '/index.md'
+      var versionData = yaml.safeLoadAll(fs.readFileSync(file))[0]
+      versionData.slug = folder
+    } catch (e) {
+      console.log('[Error] reading file ' + file)
+      return
+    }
+    if (data[versionData.slug]) {
+      data[versionData.slug].push(versionData)
+    } else {
+      data[versionData.slug] = [versionData]
+    }
+  })
+})
+
+
+// Write top level components file
+var components = Object.keys(data).map(key => {
+  return {component_name: key}
+})
+fs.writeFileSync(
+  'components.json',
+  JSON.stringify(components)
+)
+
+// Write assets to file
+var assets = []
+function  mtyper(type) {
+  switch (type) {
+    case 'wide image':
+      return 'wideImage'
+    case 'narrow image':
+      return 'narrowImage'
+  }
+}
+Object.values(data).forEach(versions => {
+  versions.forEach((version, vi) => {
+    if (!version.blocks) return
+
+    version.blocks.forEach((block, bi) => {
+      if (block.type == 'two column' && block.contents) {
+        block.contents.forEach(mediaItem => {
+          if (mediaItem.type.indexOf('image') === -1 &&
+              mediaItem.type.indexOf('video') === -1) {
+            return
+          }
+
+          var url = '/c/' +
+                    version.slug +
+                    '/v' +
+                    version.version +
+                    '/' +
+                    mediaItem.src
+
+          var req = http.request({
+            hostname: 'uxframework.pearson.com',
+            path: encodeURI(url),
+            method: 'HEAD'
+          }, res => {
+            if (res.statusCode >= 300) {
+              console.log(`[${res.statusCode}] ${version.title} ${version.version} ${encodeURI(url)}`)
+            } else {
+
+              assets.push({
+                file: `http://uxframework.pearson.com${url}`,
+                folder: slugify(version.title) + '-v' + version.version,
+                caption: mediaItem.caption || '',
+                mediaType: mtyper(mediaItem.type),
+                border: mediaItem.border || false
+              })
+
+              if (
+                vi == versions.length - 1 &&
+                bi == version.blocks.length - 1
+              ) {
+                fs.writeFileSync(
+                  'assets.json',
+                  JSON.stringify(assets)
+                )
+              }
+            }
+          })
+
+          req.on('error', function(e) {
+            console.log(e, `${version.title} ${version.version} ${mediaItem.src}`)
+          })
+          req.end()
+        })
+      }
+    })
+  })
+})
---
+++ exporter_versions.js
@@ -0,0 +1,125 @@
+var fs = require('fs')
+var yaml = require('js-yaml')
+var marked = require('marked')
+
+// read in parent IDs
+var parentIDs = JSON.parse(fs.readFileSync('./component_ids.json'))
+var folderIDs = JSON.parse(fs.readFileSync('./folder_ids.json'))
+
+// Create core data structure
+var data = {}
+
+var folders = fs.readdirSync('./c')
+folders.forEach(folder => {
+  if (folder == '.DS_Store') return
+
+  var versions = fs.readdirSync('./c/' + folder)
+
+  versions.forEach(version => {
+    if (version.indexOf('v') !== 0) return
+
+    try {
+      var file = './c/' + folder + '/' + version + '/index.md'
+      var versionData = yaml.safeLoadAll(fs.readFileSync(file))[0]
+      versionData.slug = folder
+    } catch (e) {
+      console.log(e)
+      console.log('[Error] reading file ' + file)
+      return
+    }
+    if (data[versionData.slug]) {
+      data[versionData.slug].push(versionData)
+    } else {
+      data[versionData.slug] = [versionData]
+    }
+  })
+})
+
+// Create version json file
+var versions = []
+function blockData(block, folder) {
+  switch (block.type) {
+    case 'two column':
+    case 'two_column':
+      var baseObj = {
+        type: 'twoColumn',
+        enabled: 1,
+        fields: {
+          text: marked(String(block.text || ""))
+        }
+      }
+
+      if (block.contents && block.contents.length > 0) {
+        baseObj.related = {
+          media: {
+            '@model': 'AssetModel',
+            matchBy: 'filename',
+            matchValue: block.contents.map(item => {
+              if (
+                item.type.indexOf('image') !== -1 ||
+                item.type.indexOf('video') !== -1
+              ) {
+                return item.src.split('/').pop()
+              } else {
+                return null
+              }
+            }).filter(Boolean),
+            matchCriteria: {
+              source: 'component_assets',
+              folderId: folderIDs[folder]
+            }
+          }
+        }
+      }
+      return baseObj
+    case 'section':
+      return {
+        type: 'sectionName',
+        enabled: 1,
+        fields: {
+          nameOfSection: block.name
+        }
+      }
+    default:
+      return {}
+  }
+}
+
+Object.keys(data).forEach(component => {
+  data[component].forEach(version => {
+    versions.push({
+      '@model': 'EntryModel',
+      attributes: {
+        sectionId: 3,
+        typeId: 5,
+        enabled: true,
+        parentId: parentIDs[component]
+      },
+      content: {
+        fields: {
+          version: version.version,
+          implementation: version.implementation,
+          tagline: marked(String(version.tagline || "")),
+          description: marked(String(version.description || "")),
+          usageGuidelines: marked(String(version.usage_guidelines || "")),
+          features: version.features && version.features.reduce((mem, feature, i) => {
+            mem[`row${i+1}`] = {col1: feature}
+            return mem
+          }, {}),
+          blocks: version.blocks && version.blocks.reduce((mem, block, i) => {
+            mem[`new${i+1}`] = blockData(
+              block,
+              `${component}-v${version.version}`
+            )
+            return mem
+          }, {})
+        }
+      }
+    })
+  })
+})
+
+fs.writeFileSync(
+  'versions.json',
+  JSON.stringify(versions)
+)
